367f63059e158a56c546d650f7f58e7cea4ab5e5
MarcianoMendes <nmendes94@hotmail.com>
2022-03-17 14:19:07 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link :to="{name:'register', params:{dado:'10'}}">Register</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <input type="email" name="email" id="email" placeholder="E-mail"><br>
+    <input type="text" name="name" id="name" placeholder="Nome"><br>
+    <button>Enviar</button>
+    <router-view/>
+  </div>
+</template>
